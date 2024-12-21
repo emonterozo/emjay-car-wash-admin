4f8dd9c9bbe2d34a8ea0b8b1a98171b620553e42
jtrzdev <johnwardenterez@gmail.com>
2024-12-21 22:52:11 +0800

    feat: add error-modal

--- src/components/ErrorModal/ErrorModal.tsx
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -1,92 +1,147 @@
-import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
-import { Modal, Card, Text, Button } from '@ui-kitten/components';
+import React /*useState*/ from 'react';
+import { StyleSheet, View, Modal, Pressable, Text, Dimensions } from 'react-native';
+// import { Modal, Card, Text, Button } from '@ui-kitten/components';
 
-import { IMAGES } from '@app/constant';
+// import { IMAGES } from '@app/constant';
+import { NetworkErrorIcon } from '@app/icons';
+// import { Text } from 'react-native-svg';
+// import { Icon } from '@ui-kitten/components';
 
 export type ErrorModalProps = {
   isVisible: boolean;
-  variant: 'network' | 'server';
+  // variant: 'network' | 'server'
   onRetry: () => void;
   onCancel: () => void;
 };
 
-const ErrorModal: React.FC<ErrorModalProps> = ({ isVisible, variant, onCancel, onRetry }) => {
+const titleText = 'Something went wrong';
+const descriptionText = 'Were actively resolving the issue. Please refresh the page and try again.';
+
+const ErrorModal: React.FC<ErrorModalProps> = ({ isVisible, onCancel, onRetry }) => {
   return (
-    <Modal visible={isVisible} backdropStyle={styles.backdrop}>
-      <Card disabled={true} style={styles.card}>
-        <View style={styles.header}>
-          <Image
-            source={variant === 'network' ? IMAGES.NO_INTERNET : IMAGES.SERVER_DOWN}
-            style={styles.image}
-            resizeMode="contain"
-          />
-          <Text category="h5" style={styles.title}>
-            {variant === 'network' ? 'No Internet Connection' : 'Something went wrong'}
-          </Text>
+    <Modal
+      visible={isVisible}
+      animationType="slide"
+      onRequestClose={() => {
+        onCancel;
+      }}
+      transparent={true}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalViewContainer}>
+          <NetworkErrorIcon />
+
+          <View style={styles.textContainer}>
+            <Text style={styles.titleText}>{titleText}</Text>
+            <Text style={styles.descriptionText}>{descriptionText}</Text>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={onCancel}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? 'rgba(1, 111, 185, 0.84)' : 'white',
+                  borderColor: pressed ? '#9A9A9A' : '#9A9A9A',
+                  borderWidth: pressed ? 1 : 1,
+                },
+                styles.buttonCardContainer,
+              ]}
+            >
+              <Text style={styles.buttonCancelText}>Cancel</Text>
+            </Pressable>
+            <Pressable
+              onPress={onRetry}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? 'rgba(255, 255, 255, 0.72)' : '#016FB9',
+                },
+                styles.buttonCardContainer,
+              ]}
+            >
+              <Text style={styles.buttonTryAgainText}>Try Again</Text>
+            </Pressable>
+          </View>
         </View>
-        <Text category="p1" appearance="hint" style={styles.description}>
-          {variant === 'network'
-            ? 'Poor network connection detected. Please check your connectivity'
-            : 'We are currently experiencing some issues with our server. Please try again'}
-        </Text>
-        <View style={styles.buttonContainer}>
-          <Button
-            style={[styles.button, styles.cancelButton]}
-            appearance="outline"
-            onPress={onCancel}
-          >
-            Cancel
-          </Button>
-          <Button style={styles.button} status="primary" onPress={onRetry}>
-            Try Again
-          </Button>
-        </View>
-      </Card>
+      </View>
+      {/* </View> */}
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  backdrop: {
-    backgroundColor: 'rgba(33, 37, 41, 0.8)',
-  },
-  card: {
-    borderRadius: 16,
-    padding: 10,
-    width: '90%',
-    alignSelf: 'center',
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
-    gap: 12,
-  },
-  image: {
-    width: 50,
-    height: 50,
-  },
-  title: {
-    flexWrap: 'wrap',
+  modalContainer: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(141, 141, 141, 0.43)',
   },
-  description: {
-    marginBottom: 24,
-    lineHeight: 20,
+
+  modalViewContainer: {
+    width: Dimensions.get('window').width * 0.9, //- 87.81, //352.29 width as per Figma
+    // height: 479.81, // height as per Figma
+    backgroundColor: 'white',
+    borderRadius: 19.12,
+    paddingVertical: 47.8,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 38.24,
+  },
+
+  textContainer: {
+    gap: 8,
+  },
+
+  titleText: {
+    fontSize: 19.12,
+    fontFamily: 'AeonikTRIAL-Bold',
+    fontWeight: 'bold',
+    color: 'black',
     textAlign: 'center',
   },
+
+  descriptionText: {
+    fontSize: 12.75,
+    fontFamily: 'AeonikTRIAL-Regular',
+    fontWeight: 'regular',
+    color: '#5C5C5C',
+    textAlign: 'center',
+  },
+
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 12,
   },
-  button: {
+
+  buttonCardContainer: {
+    paddingHorizontal: 9.56,
+    paddingVertical: 12.75,
+    borderRadius: 47.8,
+    width: 148.18,
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
-    marginHorizontal: 8,
   },
-  cancelButton: {
-    backgroundColor: 'transparent',
+
+  buttonCancelText: {
+    fontSize: 19.12,
+    fontFamily: 'AeonikTRIAL-Regular',
+    fontWeight: 'regular',
+    textAlign: 'center',
+    color: '#016FB9',
+    lineHeight: 19.72,
+  },
+
+  buttonTryAgainText: {
+    fontSize: 19.12,
+    fontFamily: 'AeonikTRIAL-Regular',
+    fontWeight: 'regular',
+    color: 'white',
+    textAlign: 'center',
+    lineHeight: 19.72,
   },
 });
 
