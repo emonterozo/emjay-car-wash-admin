fbfdb956ecadde1a74088efe59100ad25f6d8c13
jtrzdev <johnwardenterez@gmail.com>
2024-12-26 23:36:09 +0800

    feat: add signin toast

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -16,7 +16,7 @@ import {
 import GlobalContext from '@app/context';
 import { IMAGES } from '@app/constant';
 import { EyeOpenIcon, EyeCloseIcon, LockIcon, UserIcon } from '@app/icons';
-import { ErrorModal, LoadingAnimation } from '@app/components';
+import { ErrorModal, LoadingAnimation, Toast } from '@app/components';
 
 const Login = () => {
   const { user, setUser } = useContext(GlobalContext);
@@ -30,27 +30,87 @@ const Login = () => {
     password: '',
   });
 
-  const login = () => {
-    setScreenStatus({ hasError: false, isLoading: false });
-
-    setTimeout(() => {
-      setUser({
-        ...user,
-        id: 'user1',
-      });
-    }, 5000);
-  };
-
   const hasNoInput = () => input.username.length === 0 || input.password.length === 0;
 
   const onChange = (key: string, text: string) => setInput({ ...input, [key]: text });
 
   const toggleSecureEntry = () => setIsPasswordSecure(!isPasswordSecure);
 
+  const login = () => {
+    const { hasError, isLoading } = screenStatus;
+
+    if (hasError) {
+      // Handle error case
+      setScreenStatus({ hasError: true, isLoading: true });
+      setTimeout(() => {
+        //set loading visibility to false
+        setScreenStatus({ hasError: true, isLoading: false });
+        handleToast('error');
+        // console.log('Error: cannot proceed with login');
+      }, 3000); // Simulate toast display duration
+    } else if (!isLoading) {
+      // Handle success case
+      setScreenStatus({ hasError: false, isLoading: true }); // Start loading
+      setTimeout(() => {
+        setScreenStatus({ hasError: false, isLoading: false }); // Stop loading
+        handleToast('success');
+
+        // Delay the setUser until the toast has finished displaying
+        setTimeout(() => {
+          // console.log('Success: login complete');
+          setUser({
+            ...user,
+            id: 'user1',
+          });
+        }, 3000);
+      }, 3000); // Simulate loading duration
+    }
+  };
+
+  type ToastDetails = {
+    visibility: boolean;
+    duration: number;
+    message: string;
+    type: 'success' | 'error' | 'info';
+  };
+  const [toastDetails, setToastDetails] = useState<ToastDetails>({
+    visibility: false,
+    duration: 0,
+    message: '',
+    type: 'info',
+  });
+
+  const handleToast = (type: 'success' | 'error' | 'info') => {
+    const message =
+      type === 'success'
+        ? 'You have Successfully logged in. Redirecting you to your dashboard now'
+        : type === 'error'
+        ? 'Oops! Seems like you input wrong details. Please try again.'
+        : 'Please input your Username and Password above to Sign In';
+
+    setToastDetails({
+      visibility: true,
+      duration: 3000,
+      message,
+      type,
+    });
+
+    setTimeout(() => {
+      setToastDetails((prev) => ({ ...prev, visibility: false }));
+    }, 3000);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <LoadingAnimation isLoading={screenStatus.isLoading} />
       <ErrorModal isVisible={false} onCancel={() => {}} onRetry={() => {}} />
+      <Toast
+        isVisible={toastDetails.visibility}
+        message={toastDetails.message}
+        duration={toastDetails.duration}
+        type={toastDetails.type}
+        onClose={() => {}}
+      />
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.avoidingView}
@@ -88,14 +148,31 @@ const Login = () => {
           <Pressable
             disabled={screenStatus.isLoading || hasNoInput()}
             style={({ pressed }) => [
-              {
-                backgroundColor: pressed ? 'rgba(1, 111, 185, 0.7)' : '#016FB9',
-              },
               styles.button,
+              screenStatus.isLoading || hasNoInput()
+                ? {
+                    backgroundColor: 'transparent',
+                    borderWidth: 1,
+                    borderColor: '#016FB9',
+                  }
+                : { backgroundColor: pressed ? 'rgba(1, 111, 185, 0.7)' : '#016FB9' },
             ]}
-            onPress={login}
+            onPress={() => login()}
           >
-            <Text style={styles.buttonText}>Sign In</Text>
+            {({ pressed }) => (
+              <Text
+                style={[
+                  styles.buttonTextColorDefault,
+                  screenStatus.isLoading || hasNoInput()
+                    ? styles.buttonTextColorDisabled // Disabled text color
+                    : pressed
+                    ? styles.buttonTextColorDefault
+                    : styles.buttonTextColorDefault, // Enabled text color and pressed state
+                ]}
+              >
+                Sign In
+              </Text>
+            )}
           </Pressable>
         </ScrollView>
       </KeyboardAvoidingView>
@@ -135,6 +212,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#5C5C5C',
     marginTop: 13,
+    lineHeight: 16,
   },
   form: {
     gap: 26,
@@ -166,12 +244,21 @@ const styles = StyleSheet.create({
     borderRadius: 49,
     justifyContent: 'center',
   },
-  buttonText: {
+  buttonTextColorDefault: {
     fontSize: 24,
     fontFamily: 'AeonikTRIAL-Regular',
     fontWeight: 'regular',
     textAlign: 'center',
     color: '#ffffffff',
+    lineHeight: 24,
+  },
+  buttonTextColorDisabled: {
+    fontSize: 24,
+    fontFamily: 'AeonikTRIAL-Regular',
+    fontWeight: 'regular',
+    textAlign: 'center',
+    color: '#016FB9',
+    lineHeight: 24,
   },
 });
 
