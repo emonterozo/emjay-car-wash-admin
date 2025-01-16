dba9fc5d9b646896ce5b7abfe8121d8b044c1866
Eric Monterozo <ericmonterozo@Erics-MacBook-Pro.local>
2025-01-17 00:13:15 +0800

    chore: version up

--- src/screens/CustomerDetails/CustomerDetails.tsx
+++ src/screens/CustomerDetails/CustomerDetails.tsx
@@ -10,7 +10,6 @@ import {
   Linking,
   StatusBar,
   Image,
-  Linking,
 } from 'react-native';
 import { format } from 'date-fns';
 import { useNavigation, useRoute } from '@react-navigation/native';
