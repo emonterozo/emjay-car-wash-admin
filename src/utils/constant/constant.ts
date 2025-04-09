8dc8c9d8e36747a356f1ebf9acf3ab5e2add7419
Eric Monterozo <ric.monterozo@gmail.com>
2025-04-09 22:31:50 +0800

    feat: remove xxl size

--- src/utils/constant/constant.ts
+++ src/utils/constant/constant.ts
@@ -190,7 +190,7 @@ export const VEHICLE_TYPES = ['Car', 'Motorcycle'];
 export const CAR_SIZES = ['Small', 'Medium', 'Large', 'Extra Large'];
 export const MOTORCYCLE_SIZES = ['Small', 'Medium', 'Large'];
 export const STATISTICS_FILTER = ['Income', 'Expenses'];
-export const SIZES = ['sm', 'md', 'lg', 'xl', 'xxl'];
+export const SIZES = ['sm', 'md', 'lg', 'xl'];
 export const SIZE_DESCRIPTION: Record<SizeKey, string> = {
   sm: 'Small',
   md: 'Medium',
